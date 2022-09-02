718b2bde07fc4f4f426f6f54bd995e84237c34f6
= <=>
2022-09-02 13:59:28 -0600

    initial commit

---
+++ settings.py
@@ -0,0 +1,50 @@
+from os import environ
+
+
+SESSION_CONFIGS = [
+    dict(
+        name='vue_integration',
+        display_name='Vue.js Integration',
+        num_demo_participants=1,
+        app_sequence=['vue_integration'],
+    ),
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True
+
+ROOMS = [
+    dict(
+        name='econ101',
+        display_name='Econ 101 class',
+        participant_label_file='_rooms/econ101.txt',
+    ),
+    dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
+]
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """
+Here are some oTree games.
+"""
+
+
+SECRET_KEY = 'jw&j1n*ox2$4nif1+mhy3ikbrevucweo0-y7ru8&&h$22rotp+'
+
+INSTALLED_APPS = ['otree']
